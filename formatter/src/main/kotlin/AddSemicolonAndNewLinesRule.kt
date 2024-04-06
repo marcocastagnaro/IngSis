e35a9d5b2ae97de546784e17e6a96168295ac7bc
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-04-05 21:22:53 -0300

    Changes

--- formatter/src/main/kotlin/AddSemicolonAndNewLinesRule.kt
+++ formatter/src/main/kotlin/AddSemicolonAndNewLinesRule.kt
@@ -1,12 +1,12 @@
 package org.example
 
-class AddSemicolonAndNewLinesRule: FormatRule {
+class AddSemicolonAndNewLinesRule(): FormatRule {
 
     override fun applyRule(tokenList: MutableList<Token>): List<Token> {
         val lastTokenPosition = tokenList[tokenList.size - 1].getFinalPosition()
         val position = Position(lastTokenPosition.getRow(), lastTokenPosition.getColumn() + 1)
         val semicolonAndNewLine = Token(Types.PUNCTUATOR, ";\n", position, position)
-        tokenList.addLast(semicolonAndNewLine)
+        tokenList.add(semicolonAndNewLine)
         return tokenList
     }
 }
