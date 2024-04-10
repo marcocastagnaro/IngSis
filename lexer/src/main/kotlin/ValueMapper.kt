f6ea6525914d9dd340b5cc33606a13c04e53bcad
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-04-10 10:57:07 -0300

    formatting changes

--- lexer/src/main/kotlin/ValueMapper.kt
+++ lexer/src/main/kotlin/ValueMapper.kt
@@ -14,8 +14,8 @@ class ValueMapper(private val assignatorTuple: List<Pair<Assignator, Types>> = d
     companion object {
         val default =
             listOf(
-                RegexAssignator("(if|else|for|while|when|fun|function|class|object|return|break|continue|let)".toRegex()) to Types.KEYWORD,
-                RegexAssignator("println".toRegex()) to Types.FUNCTION,
+                RegexAssignator("(if|else|for|while|when|fun|function|class|object|return|break|continue|let|const)".toRegex()) to Types.KEYWORD,
+                RegexAssignator("println|readInput".toRegex()) to Types.FUNCTION,
                 RegexAssignator("(string|number)".toRegex()) to Types.DATA_TYPE,
                 RegexAssignator("(:)".toRegex()) to Types.DECLARATOR,
                 RegexAssignator("(=)".toRegex()) to Types.ASSIGNATION,
