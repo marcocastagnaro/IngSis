837ddaa091abb95f7bd86ae62625181f75e05e00
Facundo <facundo.sydransky@ing.austral.edu.ar>
2024-04-11 17:54:33 -0300

    feature: sca output

--- staticCodeeAnalyzer/src/main/kotlin/sca/Sca.kt
+++ staticCodeeAnalyzer/src/main/kotlin/sca/Sca.kt
@@ -1,9 +1,10 @@
 package org.example.sca
 
 import org.example.AbstractSyntaxTree
+import output.Output
 
 interface Sca {
     fun readJson(rulesPath: String)
 
-    fun check(trees: List<AbstractSyntaxTree>)
+    fun check(trees: List<AbstractSyntaxTree>): Output
 }
