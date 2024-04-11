3e5563f0ae4cc99233f10c90794b73d989cb0783
quinhos <marco.castagnaro@ing.austral.edu.ar>
2024-04-11 15:45:24 -0300

    ktlint

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "Ingsis"
+rootProject.name = "ingSis"
 include("AST", "lexer", "parser", "Token")
 include("interpreter")
 include("formatter")
